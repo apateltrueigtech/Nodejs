111af34b55c8a35bfcd1ea5e6989a90cc2547424
apateltrueigtech <apateltrueigtech.com>
2025-02-13 10:57:00 +0530

    server

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const http = require("http");
+const fs = require("fs")
+
+const myServer =http.createServer((req, res)=>{
+    const log = `${Date.now()}: ${req.url} New Req Received\n`;
+    fs.appendFile("log.txt",log,(err,data)=>{
+        // res.end("Hello From Server");
+        switch(req.url){
+            case '/': res.end("HomePage")
+            break
+            case '/about' : res.end("I am Aditi Patel");
+            break;
+            default:
+                res.end("404 Not Found");
+        
+        }
+    })
+    // console.log("New Req Received.");
+ 
+});
+myServer.listen(8000,()=> console.log("Server Started"));
