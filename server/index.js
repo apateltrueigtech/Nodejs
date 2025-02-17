ba5e22a4ee4da6c008f6ff0f40f6f31c9a6a0fe8
apateltrueigtech <apateltrueigtech.com>
2025-02-17 12:04:24 +0530

    done

--- server/index.js
+++ server/index.js
@@ -1,38 +1,52 @@
 const http = require("http");
-const fs = require("fs");
-const url = require("url");
+// const fs = require("fs");
+// const url = require("url");
+const express = require("express");
+
+const app = express();
+
+app.get('/',(req,res)=>{
+    return res.send("Hello from Home Page");
+})
+app.get('/about',(req,res)=>{
+    return res.send("Hello About  Page");
+})
+
+function myHandler(req,res){
+
+}
 
 
-const myServer =http.createServer((req, res)=>{
-    if(req.url === "/favicon.ico") return res.end();
-    const log = `${Date.now()}: ${req.method} ${req.url} New Req Received\n`;
-    const myUrl = url.parse(req.url,true);
-    console.log(myUrl);
-    fs.appendFile("log.txt",log,(err,data)=>{
-        // res.end("Hello From Server");
-        switch(myUrl.pathname){
-            case '/':
-                if(req.method==='GET') res.end("HomePage")
-            break
-            case '/about': 
-            const username = myUrl.query.myname;
-            res.end(`Hi , ${username}`);
-            break;
-            case '/search':
-                const search = myUrl.query.search_query;
-                res.end('Here are your results' + search);
-            case "/signup":
-                if(req.method==='GET') res.end("This is a signup Form");
-                else if(req.method==="POST"){
-                    //DB Query
-                    res.end("Success")
-                }          
-                default:
-                res.end("404 Not Found");
+// const myServer =http.createServer((app)=>{
+//     if(req.url === "/favicon.ico") return res.end();
+//     const log = `${Date.now()}: ${req.method} ${req.url} New Req Received\n`;
+//     const myUrl = url.parse(req.url,true);
+//     console.log(myUrl);
+//     fs.appendFile("log.txt",log,(err,data)=>{
+//         // res.end("Hello From Server");
+//         switch(myUrl.pathname){
+//             case '/':
+//                 if(req.method==='GET') res.end("HomePage")
+//             break
+//             case '/about': 
+//             const username = myUrl.query.myname;
+//             res.end(`Hi , ${username}`);
+//             break;
+//             case '/search':
+//                 const search = myUrl.query.search_query;
+//                 res.end('Here are your results' + search);
+//             case "/signup":
+//                 if(req.method==='GET') res.end("This is a signup Form");
+//                 else if(req.method==="POST"){
+//                     //DB Query
+//                     res.end("Success")
+//                 }          
+//                 default:
+//                 res.end("404 Not Found");
         
-        }
-    })
-    // console.log("New Req Received.");
+        //  }
+        //  )
+//     // console.log("New Req Received.");
  
-});
-myServer.listen(8000,()=> console.log("Server Started"));
+// });
+app.listen(8000,()=> console.log("Server Started"));
