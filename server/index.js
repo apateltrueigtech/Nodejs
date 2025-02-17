90166f52b16817d3d0718ba682f47322ce551802
apateltrueigtech <apateltrueigtech.com>
2025-02-17 09:23:37 +0530

    server

--- server/index.js
+++ server/index.js
@@ -1,16 +1,32 @@
 const http = require("http");
-const fs = require("fs")
+const fs = require("fs");
+const url = require("url");
 
 const myServer =http.createServer((req, res)=>{
-    const log = `${Date.now()}: ${req.url} New Req Received\n`;
+    if(req.url === "/favicon.ico") return res.end();
+    const log = `${Date.now()}: ${req.method} ${req.url} New Req Received\n`;
+    const myUrl = url.parse(req.url,true);
+    console.log(myUrl);
     fs.appendFile("log.txt",log,(err,data)=>{
         // res.end("Hello From Server");
-        switch(req.url){
-            case '/': res.end("HomePage")
+        switch(myUrl.pathname){
+            case '/':
+                if(req.method==='GET') res.end("HomePage")
             break
-            case '/about' : res.end("I am Aditi Patel");
+            case '/about': 
+            const username = myUrl.query.myname;
+            res.end(`Hi , ${username}`);
             break;
-            default:
+            case '/search':
+                const search = myUrl.query.search_query;
+                res.end('Here are your results' + search);
+            case "/signup":
+                if(req.method==='GET') res.end("This is a signup Form");
+                else if(req.method==="POST"){
+                    //DB Query
+                    res.end("Success")
+                }          
+                default:
                 res.end("404 Not Found");
         
         }
