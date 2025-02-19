2f1cca5d6970075855005faf7e6d0d367b5a1b88
apateltrueigtech <apateltrueigtech.com>
2025-02-19 15:27:50 +0530

    done

--- server/index.js
+++ server/index.js
@@ -1,10 +1,49 @@
 const express = require("express");
+const users =require('./MOCK_DATA.json')
 const app = express();
 const PORT =8000;
 
 
 // Routes
+app.get('/users',(req,res)=>{
+    const html =`
+    <ul>
+    ${users.users.map(user => `<li>${user.id}</li>`)}
+    </ul>
+    `;
+    res.send(html);
+})
 
+//REST API
+
+
+app.get('/api/users',(req,res)=>{
+    return res.json(users);
+});
+
+app.get("/api/users/:id",(req,res)=>{
+    // console.log(users)
+        const id = Number(req.params.id);
+        const user = users.users.find((user)=> user.id === id);
+        // if(!user){
+        //     return res.status(404).json({error:"User not found"});
+        // }
+        return res.json(user);
+});
+  
+    
+
+app.post('/api/users',(req,res)=>{
+   return res.json({status:"pending"});
+});
+
+app.patch('/api/users/:id',(req,res)=>{
+    return res.json({status:"pending"});
+ });
+
+app.delete('/api/users/:id',(req,res)=>{
+    return res.json({status:"pending"});
+});
 
 app.listen(PORT,()=>console.log(`Server Started at PORT:${PORT}`))
 
